4f105985f83df6eb3688052e2965ada545b2827e
rschmitz <mail@roberto-schmitz.de>
2015-01-07 17:35:43 +0100

    add app to repo

---
+++ app/jquerymobile/www/js/jquery.mobile_preset.js
@@ -0,0 +1,10 @@
+var onDeviceReady=function(){
+/* convert ul into listview */
+$("[data-role=panel] ul").listview();
+
+/* external panel is used in the demo
+   therefore, it should be enhanced manually
+   in addition to all contents inside it */
+$("[data-role=panel]").enhanceWithin().panel();
+
+};
